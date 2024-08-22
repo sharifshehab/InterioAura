d5824de64e8c5b8e1985bf26567a484928d2eaad
Shrafi Shehab <svshuvo696@gmail.com>
2024-08-22 16:19:39 +0600

    navbar complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,7 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {
-            colors: {
-                primary: '#ABEF5F',
-                primaryV2: '#ABEF5F80',
-                primaryV3: '#ABEF5F33',
-                secondary: '#212121',
-                secondaryV2: '#494949',
-                plainText: '#737373'
-            }
-        },
+        extend: {},
     },
     plugins: [],
 }
